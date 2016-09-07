ee17832e965072a9b8d97323f9d31bdef54d7cfa
forgere <573596314@qq.com>
2016-09-07 21:10:12 +0800

    first form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-	config.generators do |g|
+  config.generators do |g|
 	  g.template_engine :slim
 	end
 end
